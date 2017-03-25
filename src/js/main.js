1ca526514edbe843370f8c47354c8cdf120114eb
craigymcl <craigymcl@hotmail.com>
2017-03-25 21:24:22 +0000

    Deleted unused fles.

--- src/js/main.js
+++ src/js/main.js
@@ -1,131 +1,48 @@
+// quotes are stored in an array of objects
+var quotes = [{
+  "attrib": "Steve Jobs",
+  "quote": "Design is not just what it looks like and feels like. Design is how it works."
+}, {
+  "attrib": "Steve Jobs",
+  "quote": "Innovation distinguishes between a leader and a follower."
+}, {
+  "attrib": "Albert Einstein",
+  "quote": "Reality is merely an illusion, albeit a very persistent one."
+}, {
+  "attrib": "Albert Einstein",
+  "quote": "If you can't explain it simply, you don't understand it well enough."
+}, {
+  "attrib": "Albert Einstein",
+  "quote": "Strive not to be a success, but rather to be of value."
+}, {
+  "attrib": "Steve Jobs",
+  "quote": "Your time is limited, so don’t waste it living someone else’s life."
+}, {
+  "attrib": "Bill Gates",
+  "quote": "Success is a lousy teacher. It seduces smart people into thinking they can't lose."
+}, {
+  "attrib": "Albert Einstein",
+  "quote": "Science without religion is lame, religion without science is blind."
+}, {
+  "attrib": "Henry Ford",
+  "quote": "Whether you think you can or you think you can’t, you’re right."
+}];
+
+// function to load and display a new quote
+function newQuote() {
+  var quoteID = Math.floor(Math.random() * (quotes.length));
+  $("#quote").html(quotes[quoteID].quote);
+  $("#attrib").html(quotes[quoteID].attrib);
+  $("#twitterButton").attr("href", "http://twitter.com/intent/tweet?text=" + quotes[quoteID].quote + " -" + quotes[quoteID].attrib);
+}
+
+// wait for page load before displaying content
 $(document).ready(function() {
-    $('.hamburgerIcon').click(function() {
-        if ($('body').hasClass('open')) {
-            $(this).removeClass('open');
-            $('body').removeClass('open');
+  // load an initial quote
+  newQuote();
 
-            $(this).text('MENU');
-			$('a').on('click', function() {
-				console.log("clicked");
-				$('body').removeClass('open');
-			});
-
-        } else {
-            $(this).addClass('open');
-            $('body').addClass('open');
-            $(this).text('Close');
-        }
-    });
-});
-
-
-
-
- $('body nav a').click(function(e) {
-	if($('.hamburgerIcon').hasClass('open')) {
-		$('body').removeClass('open');
-		$('.hamburgerIcon').text('MENU');
-	}
-});
-
-
-
-/*
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('.mainNav').outerHeight();
-
-
-
-$(window).scroll(function(event) {
-    didScroll = true;
-});
-
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-
-    // Make sure they scroll more than delta
-    if (Math.abs(lastScrollTop - st) <= delta)
-        return;
-
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight) {
-        // Scroll Down
-        $('.mainNav').removeClass('mainNav--navDown').addClass('mainNav--navUp');
-    } else {
-        if ($(window).scrollTop() == 0) {
-            $('.mainNav').removeClass('mainNav--navDown mainNav--navUp');
-        }
-        // Scroll Up
-        else if (st + $(window).height() < $(document).height()) {
-            $('.mainNav').removeClass('mainNav--navUp').addClass('mainNav--navDown');
-        }
-    }
-
-    lastScrollTop = st;
-};
-
-*/
-
-var previousScroll = 0;
-$(window).scroll(function(event){
-   var scroll = $(this).scrollTop();
-   if (scroll > previousScroll){
-       $('.mainNav').removeClass('mainNav--navDown').addClass('mainNav--navUp');
-   } else if(scroll < 150) {
-       $('.mainNav').removeClass('mainNav--navDown mainNav--navUp');
-       console.log("test");
-   } else {
-      $('.mainNav').removeClass('mainNav--navDown').addClass('mainNav--navDown');
-   }
-   previousScroll = scroll;
-});
-
-
-$('#getInTouchForm').validate();
-
-
-$('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 1000);
-            return false;
-        }
-    }
-});
-
-
-
-
-var signupForm = $('#getInTouchForm');
-signupForm.on('submit', function(e) {
-    e.preventDefault();
-    if ($('#getInTouchForm').valid()) {
-        $.ajax({
-            url: 'mail.php',
-            method: 'POST',
-            data: signupForm.serialize(),
-            success: function() {
-                $('#contactUs').addClass('fullWidth--fullHeight fullWidth--flex');
-                $('#contactUs .cpIntro').addClass("cpIntro--textLarge");
-                $('#contactUs .cpIntro h2').text("Thank you!");
-                $('#contactUs .cpIntro p').html("We'll be in touch very soon. <a href='index.html#contactUs'>Send another message</a>");
-                $(signupForm).hide();
-            }
-        })
-	}
+  // retrieve a new quote when the button is clicked
+  $("#generateButton").on("click", function() {
+    newQuote();
+  });
 });
